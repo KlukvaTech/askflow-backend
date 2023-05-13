6e08be4666db73af3ad2f6335b5d5a195d968da9
Codemurt <egor.lebe@inbox.ru>
2023-05-13 16:10:31 +0500

    remove comments

--- app.py
+++ app.py
@@ -11,21 +11,15 @@ import re
 from string import punctuation
 from time import sleep
 import trafilatura
-#from pymystem3 import Mystem
 
 app = Flask(__name__)
 TOKEN = os.getenv('TOKEN')
 TOKEN_SPACE = os.getenv('TOKEN_SPACE')
-#API_URL = "https://api-inference.huggingface.co/models/AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru"
 API_URL = "https://codemurt-qa-model2.hf.space/api/predict"
-# BEARER = "Bearer " + TOKEN
-# headers = {"Authorization": BEARER}
 BEARER_SPACE = "Bearer " + TOKEN_SPACE
 headers = {"Authorization": BEARER_SPACE, "Content-Type": "application/json"}
 
 model = compress_fasttext.models.CompressedFastTextKeyedVectors.load('model/geowac_tokens_sg_300_5_2020-100K-20K-100.bin')
-
-#m = Mystem()
 
 logging.basicConfig(level=logging.INFO)
 
@@ -42,9 +36,6 @@ def kl_tokenize(sentence):
     tokens = [_.text for _ in list(tokenize(sentence))]
     res = [token for token in tokens if token not in punctuation]
     return res
-
-# def kl_stemming(sentence):
-#     return ''.join(m.lemmatize(sentence))
 
 def cosine(u, v):
     if np.isnan(u).any() or np.isnan(v).any(): 
@@ -64,79 +55,6 @@ def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
-# @app.route('/onlytext', methods=["POST"])
-# def echo_only_text():
-#     try:
-#         req = request.get_json()
-#         logging.info(f'onlytext: request: {req}')
-#         txt = req['text']
-#         question = req['question']
-#     except:
-#         logging.info(f'Failed. Incorrect input')
-#         return "Incorrect input"
-
-#     #query({"inputs": {"question": "Turn", "context": "Turn! Turn! Turn!"}})
-
-#     txt = txt.replace('\n', '. ')
-#     lst = [_.text for _ in list(sentenize(txt))]
-#     new_lst = []
-#     for sent in lst:
-#         new_lst.append(kl_preprocess(sent))
-#     new_lst = [x for x in new_lst if x]
-#     embedded_data = [(embed(new_lst[i]), i) for i in range(len(new_lst))]
-    
-#     indexes = set()
-
-#     # def add_idx_to_set(idx):
-#     #     idx = int(idx)
-#     #     for i in range(idx - 1, idx + 2):
-#     #         if 0 <= i < len(lst):
-#     #             indexes.add(i)
-
-#     def get_result(text):
-#         query = embed(text)
-
-#         cosines = [(cosine(x[0], query), x[1]) for x in embedded_data]
-#         print("got cosines")
-
-#         vals = sorted(cosines, key=lambda x: x[0])
-#         idx_ans = int(vals[-1][1])
-#         #add_idx_to_set(idx_ans)
-#         indexes.add(idx_ans)
-    
-#     get_result(kl_preprocess(question))
-
-#     def get_context(set_indexes):
-#         ctx = ""
-#         for el in set_indexes:
-#             ctx += new_lst[el]
-#             ctx += " "
-#         return ctx
-    
-#     context = get_context(indexes)
-
-#     output = True
-#     while output:
-#         # res = query({
-#         #     "inputs": {
-#         #         "question": question,
-#         #         "context": context
-#         #     }
-#         # })
-#         res = query({
-#             "data": [question, context]
-#         })
-#         logging.info(f'onlytext: send request: {res}')
-#         output = 'error' in res.keys()
-#         if output:
-#             sleep(3)
-#     res['context'] = context.strip()
-#     #res['answer'] = res['answer'].strip()
-#     res['answer'] = res['data'][0].strip()
-#     logging.info(f'onlytext: return answer: {res}')
-#     indexes.clear()
-#     return res
-
 @app.route('/onlyhtml', methods=["POST"])
 def echo_only_html():
     try:
@@ -148,27 +66,14 @@ def echo_only_html():
         logging.info(f'Failed. Incorrect input')
         return "Incorrect input"
 
-    #query({"inputs": {"question": "Turn", "context": "Turn! Turn! Turn!"}})
-
     txt = trafilatura.extract(html_req)
     txt = txt.replace('\n', ' ')
-    # new_txt = txt[0]
-    # for i in range(1, len(txt)):
-    #     if txt[i] == '\n' and (txt[i - 1] != '.' or txt[i - 1] != '!' or txt[i - 1] != '?'):
-    #         new_txt += ". "
-    #     elif txt[i] == '\n':
-    #         new_txt += " "
-    #     else:
-    #         new_txt += txt[i]
+    
     lst = [_.text for _ in list(sentenize(txt))]
     new_lst = []
     for sent in lst:
         new_lst.append(kl_preprocess(sent))
 
-    # stemmed_lst = []
-    # for sent in new_lst:
-    #     stemmed_lst.append(kl_stemming(sent))
-    #new_lst = [x for x in new_lst if x]
     embedded_data = [(embed(new_lst[i]), i) for i in range(len(new_lst))]
     
     indexes = set()
@@ -198,15 +103,10 @@ def echo_only_html():
                 sleep(3)
         return res['data'][0]
 
-    
-    
     def get_result(text):
         query = embed(text)
-        # main_res = None
-        # main_ctx = None
         res_lst = []
         cosines = [(cosine(x[0], query), x[1]) for x in embedded_data]
-        # mx_score = -1.0    
         vals = sorted(cosines, key=lambda x: x[0], reverse=True)
         for cos, cos_idx in vals[:3]:
             add_idx_to_set(int(cos_idx))
@@ -214,15 +114,8 @@ def echo_only_html():
             indexes.clear()
             curr_res = send_request(curr_ctx)
             res_lst.append((curr_res, curr_ctx))
-            # if curr_res['score'] > mx_score:
-            #     main_res = curr_res
-            #     mx_score = curr_res['score']
-            #     main_ctx = curr_ctx
-        # return main_res, main_ctx
         return res_lst
-        #indexes.add(idx_ans)
-    
-    # main_res, main_ctx = get_result(kl_preprocess(question))
+        
     final_res_lst = get_result(kl_preprocess(question))
 
     def clean_sent(sent):
@@ -236,13 +129,6 @@ def echo_only_html():
                 break
         return sent
     
-    # ctx_lst = [_.text for _ in list(sentenize(main_ctx))]
-    # main_res['answer'] = clean_sent(main_res['answer'])
-    # for ctx_sent in ctx_lst:
-    #     if main_res['answer'] in ctx_sent:
-    #         ctx_sent = clean_sent(ctx_sent)
-    #         main_res['context'] = ctx_sent.strip()
-    #         break
     for ctx_ans in final_res_lst:
         ctx_lst = [_.text for _ in list(sentenize(ctx_ans[1]))]
         ctx_ans[0]['answer'] = clean_sent(ctx_ans[0]['answer'])
@@ -253,160 +139,11 @@ def echo_only_html():
                 ctx_ans[0]['context'] = ctx_sent.strip()
                 break
                 
-    #res['answer'] = res['answer'].strip()
-    # main_res['answer'] = main_res['answer'].strip()
     return_final_res_lst = [_[0] for _ in final_res_lst]
     return_final_res_lst.sort(reverse=True, key=lambda x: x['score'])
     logging.info(f'onlytext: return answer: {return_final_res_lst}')
     indexes.clear()
     return return_final_res_lst
-
-# @app.route('/onlyhtml_bert', methods=["POST"])
-# def echo_only_html_bert():
-#     try:
-#         req = request.get_json()
-#         html_req = req['html']
-#         question = req['question']
-#         logging.info(f'onlytext: request: {question}')
-#     except:
-#         logging.info(f'Failed. Incorrect input')
-#         return "Incorrect input"
-
-#     #query({"inputs": {"question": "Turn", "context": "Turn! Turn! Turn!"}})
-
-#     txt = trafilatura.extract(html_req)
-#     txt = txt.replace("\n", " ")
-#     # new_txt = txt[0]
-#     # for i in range(1, len(txt)):
-#     #     if txt[i] == '\n' and (txt[i - 1] != '.' or txt[i - 1] != '!' or txt[i - 1] != '?'):
-#     #         new_txt += ". "
-#     #     elif txt[i] == '\n':
-#     #         new_txt += " "
-#     #     else:
-#     #         new_txt += txt[i]
-#     lst = [_.text for _ in list(sentenize(txt))]
-#     new_lst = []
-#     for sent in lst:
-#         new_lst.append(kl_preprocess(sent))
-#     #new_lst = [x for x in new_lst if x]
-
-#     def mean_pooling(model_output, attention_mask):
-#         token_embeddings = model_output.last_hidden_state #First element of model_output contains all token embeddings
-#         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-#         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-
-#     #Encode text
-#     def encode(texts):
-#         # Tokenize sentences
-#         encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
-
-#         # Compute token embeddings
-#         with torch.no_grad():
-#             model_output = model_rubert(**encoded_input, return_dict=True)
-
-#         # Perform pooling
-#         embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-
-#         # Normalize embeddings
-#         embeddings = F.normalize(embeddings, p=2, dim=1)
-        
-#         return embeddings
-    
-#     preprocessed_question = kl_preprocess(question)
-#     query_emb = encode(preprocessed_question)
-#     doc_emb = encode(new_lst)
-
-#     scores = torch.mm(query_emb, doc_emb.transpose(0, 1))[0].cpu().tolist()
-
-#     doc_score_pairs = list(zip(new_lst, scores, range(len(new_lst))))
-#     doc_score_pairs = sorted(doc_score_pairs, key=lambda x: x[1], reverse=True)
-
-#     indexes = set()
-
-#     def add_idx_to_set(idx):
-#         idx = int(idx)
-#         for i in range(idx - 2, idx + 3):
-#             if 0 <= i < len(lst):
-#                 indexes.add(i)
-
-#     def get_context(set_indexes):
-#         ctx = ""
-#         for el in set_indexes:
-#             ctx += lst[el]
-#             ctx += " "
-#         return ctx
-
-#     def send_request(context):
-#         output = True
-#         while output:
-#             res = query({
-#                 "data": [question, context]
-#             })
-#             logging.info(f'onlytext: send request: {res}')
-#             output = 'error' in res.keys()
-#             if output:
-#                 sleep(3)
-#         return res
-
-#     mx_score = -1.0
-#     for doc, score, doc_idx in doc_score_pairs[:5]:
-#         add_idx_to_set(doc_idx)
-#         curr_ctx = get_context(indexes)
-#         indexes.clear()
-#         curr_res = send_request(curr_ctx)
-#         if curr_res['score'] > mx_score:
-#             main_res = curr_res
-#             mx_score = curr_res['score']
-#             main_ctx = curr_ctx
-    
-#     response_output = main_res['data'][0]
-#     response_output['context'] = main_ctx.strip()
-#     response_output['answer'] = response_output['answer'].strip()
-#     logging.info(f'onlytext: return answer: {main_res}')
-#     return response_output
-
-# @app.route('/allcontext', methods=["POST"])
-# def all_context():
-#     try:
-#         req = request.get_json()
-#         logging.info(f'onlytext: request: {req}')
-#         txt = req['text']
-#         question = req['question']
-#     except:
-#         logging.info(f'Failed. Incorrect input')
-#         return "Incorrect input"
-#     txt = txt.replace('\n', '. ')
-#     lst = [_.text for _ in list(sentenize(txt))]
-#     new_lst = []
-#     for sent in lst:
-#         new_lst.append(kl_preprocess(sent))
-#     new_lst = [x for x in new_lst if x]
-#     context = " ".join(new_lst)
-#     output = True
-#     while output:
-#         res = query({
-#             "data": [question, context]
-#         })
-#         logging.info(f'onlytext: send request: {res}')
-#         output = 'error' in res.keys()
-#         if output:
-#             sleep(3)
-#     res['context'] = txt.strip()
-#     res['answer'] = res['data'][0].strip()
-#     logging.info(f'onlytext: return answer: {res}')
-
-#     return res
-
-@app.route('/baobab', methods=["POST"])
-def baobab_text():
-    dt = request.get_json()
-    d = {'LOL KEK' : dt}
-    return jsonify(d)
-
-@app.route('/testing', methods=["POST"])
-def answer_extension():
-    d = {'LOL KEK' : "CHEBUREK"}
-    return jsonify(d)
 
 @app.route('/', methods=["GET"])
 def hello_world():
